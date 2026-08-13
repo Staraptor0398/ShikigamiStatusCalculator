9f7f466bd8848cd0e23ed99bcfcff8241a5e37c1
Staraptor0398 <281810797+Staraptor0398@users.noreply.github.com>
2026-08-14 00:21:43 +0900

    Normalize naming in StatusComparisonResultForm

--- Gui/Form/StatusComparisonResultForm.cs
+++ Gui/Form/StatusComparisonResultForm.cs
@@ -4,24 +4,24 @@ namespace Gui.Form
 {
 	public partial class StatusComparisonResultForm : System.Windows.Forms.Form
 	{
-		private readonly string _baseSnapshotName;
-		private readonly string _targetSnapshotName;
-		private readonly StatusComparisonResultDto _comparisonResult;
+		private readonly string mBaseSnapshotName;
+		private readonly string mTargetSnapshotName;
+		private readonly StatusComparisonResultDto mComparisonResult;
 
 
 		public StatusComparisonResultForm(string baseSnapshotName, string targetSnapshotName, StatusComparisonResultDto comparisonResult)
 		{
 			InitializeComponent();
 
-			_baseSnapshotName = baseSnapshotName;
-			_targetSnapshotName = targetSnapshotName;
-			_comparisonResult = comparisonResult;
+			mBaseSnapshotName = baseSnapshotName;
+			mTargetSnapshotName = targetSnapshotName;
+			mComparisonResult = comparisonResult;
 		}
 
 		private void StatusComparisonResultForm_Load(object sender, System.EventArgs e)
 		{
-			lblBaseSnapshotName.Text = _baseSnapshotName;
-			lblTargetSnapshotName.Text = _targetSnapshotName;
+			lblBaseSnapshotName.Text = mBaseSnapshotName;
+			lblTargetSnapshotName.Text = mTargetSnapshotName;
 
 			initializeComparisonResultGrid();
 			displayComparisonResult();
@@ -49,14 +49,14 @@ namespace Gui.Form
 		{
 			dgvComparisonResult.Rows.Clear();
 
-			addComparesonResultRow("攻撃力", _comparisonResult.AttackDifference);
-			addComparesonResultRow("HP", _comparisonResult.HpDifference);
-			addComparesonResultRow("防御力", _comparisonResult.DefenseDifference);
-			addComparesonResultRow("素早さ", _comparisonResult.SpeedDifference);
-			addComparesonResultRow("会心率", _comparisonResult.CriticalRateDifference);
-			addComparesonResultRow("会心DMG", _comparisonResult.CriticalDamageDifference);
-			addComparesonResultRow("効果命中", _comparisonResult.EffectHitDifference);
-			addComparesonResultRow("効果抵抗", _comparisonResult.EffectResistDifference);
+			addComparesonResultRow("攻撃力", mComparisonResult.AttackDifference);
+			addComparesonResultRow("HP", mComparisonResult.HpDifference);
+			addComparesonResultRow("防御力", mComparisonResult.DefenseDifference);
+			addComparesonResultRow("素早さ", mComparisonResult.SpeedDifference);
+			addComparesonResultRow("会心率", mComparisonResult.CriticalRateDifference);
+			addComparesonResultRow("会心DMG", mComparisonResult.CriticalDamageDifference);
+			addComparesonResultRow("効果命中", mComparisonResult.EffectHitDifference);
+			addComparesonResultRow("効果抵抗", mComparisonResult.EffectResistDifference);
 
 		}
 
